4d3de6973094b3ba4160e075fc6827f5a3828f01
Shaunak Kishore <kshaunak@gmail.com>
2022-02-21 21:51:26 -0500

    Get renderer partially working

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,113 @@
+const assert = (x: boolean, message?: () => string) => {
+  if (x) return;
+  throw new Error(message ? message() : 'Assertion failed!');
+};
+
+const drop = <T>(xs: T[], x: T): void => {
+  for (let i = 0; i < xs.length; i++) {
+  }
+};
+
 const nonnull = <T>(x: T | null, message?: () => string): T => {
   if (x !== null) return x;
   throw new Error(message ? message() : 'Unexpected null!');
 };
 
+//////////////////////////////////////////////////////////////////////////////
+
+const Constants = {
+  CHUNK_SIZE: 32,
+  CHUNK_KEY_BITS: 8,
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
 class Container {
-  _element: Element;
-  _canvas: Element;
+  element: Element;
+  canvas: HTMLCanvasElement;
 
   constructor(id: string) {
-    this._element = nonnull(document.getElementById(id), () => id);
-    this._canvas = nonnull(this._element.querySelector('canvas'));
+    this.element = nonnull(document.getElementById(id), () => id);
+    this.canvas = nonnull(this.element.querySelector('canvas'));
+  }
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+type Octree = BABYLON.Octree<BABYLON.Mesh>;
+type OctreeBlock = BABYLON.OctreeBlock<BABYLON.Mesh>;
+
+class Renderer {
+  engine: BABYLON.Engine;
+  scene: BABYLON.Scene;
+  octree: Octree;
+  blocks: Map<number, OctreeBlock>;
+
+  constructor(container: Container) {
+    const antialias = true;
+    const options = {preserveDrawingBuffer: true};
+    this.engine = new BABYLON.Engine(container.canvas, antialias, options);
+    this.scene = new BABYLON.Scene(this.engine);
+
+    const scene = this.scene;
+    scene._addComponent(new BABYLON.OctreeSceneComponent(scene));
+    this.octree = new BABYLON.Octree(() => {});
+    scene._selectionOctree = this.octree;
+    this.blocks = new Map();
+  }
+
+  addMesh(mesh: BABYLON.Mesh, dynamic?: boolean) {
+    if (dynamic) {
+      const meshes = this.octree.dynamicContent;
+      mesh.onDisposeObservable.add(() => drop(meshes, mesh));
+      meshes.push(mesh);
+      return;
+    }
+
+    const key = this.getMeshKey(mesh);
+    const block = this.getMeshBlock(mesh, key);
+    mesh.onDisposeObservable.add(() => {
+      drop(block.entries, mesh);
+      if (block.entries.length) return;
+      drop(this.octree.blocks, block);
+      this.blocks.delete(key);
+    });
+    block.entries.push(mesh);
+
+    mesh.alwaysSelectAsActiveMesh = true;
+    mesh.freezeWorldMatrix();
+    mesh.freezeNormals();
+  }
+
+  getMeshKey(mesh: BABYLON.Mesh): number {
+    assert(!mesh.parent);
+    const pos = mesh.position;
+    const mod = Constants.CHUNK_SIZE;
+    assert(pos.x % mod === 0);
+    assert(pos.y % mod === 0);
+    assert(pos.z % mod === 0);
+
+    const bits = Constants.CHUNK_KEY_BITS;
+    const mask = (1 << bits) - 1;
+    return (((pos.x / mod) & mask) << (0 * bits)) |
+           (((pos.y / mod) & mask) << (1 * bits)) |
+           (((pos.z / mod) & mask) << (2 * bits));
+  }
+
+  getMeshBlock(mesh: BABYLON.Mesh, key: number): OctreeBlock {
+    const cached = this.blocks.get(key);
+    if (cached) return cached;
+
+    const pos = mesh.position;
+    const mod = Constants.CHUNK_SIZE;
+    const min = new BABYLON.Vector3(pos.x, pos.y, pos.z);
+    const max = new BABYLON.Vector3(pos.x + mod, pos.y + mod, pos.z + mod);
+
+    const block: OctreeBlock =
+      new BABYLON.OctreeBlock(min, max, 0, 0, 0, () => {});
+    this.octree.blocks.push(block);
+    this.blocks.set(key, block);
+    return block;
   }
 };
 
