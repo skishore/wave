a4ad07c4e101952c93853a03be59e4f31e4da6d1
Shaunak Kishore <kshaunak@gmail.com>
2022-12-04 15:55:08 -0500

    OpenSimplex2D optimizations

--- lib/open-simplex-2d.ts
+++ lib/open-simplex-2d.ts
@@ -10,49 +10,8 @@ const STRETCH_2D = (1 / Math.sqrt(2 + 1) - 1) / 2;
 
 export type Noise2D = (x: number, y: number) => number;
 
-interface Contribution2D {
-  dx: number;
-  dy: number;
-  next: Contribution2D | null;
-  xsb: number;
-  ysb: number;
-}
-
-function contribution2D(
-  multiplier: number,
-  xsb: number,
-  ysb: number,
-): Contribution2D {
-  return {
-    dx: -xsb - multiplier * SQUISH_2D,
-    dy: -ysb - multiplier * SQUISH_2D,
-    next: null,
-    xsb,
-    ysb,
-  };
-}
-
 export function makeNoise2D(clientSeed: number): Noise2D {
-  const contributions: Contribution2D[] = [];
-  for (let i = 0; i < p2D.length; i += 4) {
-    const baseSet = base2D[p2D[i]];
-    let previous: Contribution2D | null = null;
-    let current: Contribution2D | null = null;
-    for (let k = 0; k < baseSet.length; k += 3) {
-      current = contribution2D(baseSet[k], baseSet[k + 1], baseSet[k + 2]);
-      if (previous === null) contributions[i / 4] = current;
-      else previous.next = current;
-      previous = current;
-    }
-    current!.next = contribution2D(p2D[i + 1], p2D[i + 2], p2D[i + 3]);
-  }
-  const lookup: Contribution2D[] = [];
-  for (let i = 0; i < lookupPairs2D.length; i += 2) {
-    lookup[lookupPairs2D[i]] = contributions[lookupPairs2D[i + 1]];
-  }
-
   const perm = new Uint8Array(256);
-  const perm2D = new Uint8Array(256);
   const source = new Uint8Array(256);
   for (let i = 0; i < 256; i++) source[i] = i;
   let seed = new Uint32Array(1);
@@ -64,7 +23,6 @@ export function makeNoise2D(clientSeed: number): Noise2D {
     r[0] = (seed[0] + 31) % (i + 1);
     if (r[0] < 0) r[0] += i + 1;
     perm[i] = source[r[0]];
-    perm2D[i] = perm[i] & 0x0e;
     source[r[0]] = source[i];
   }
 
@@ -87,26 +45,37 @@ export function makeNoise2D(clientSeed: number): Noise2D {
 
     const inSum = xins + yins;
     const hash = (xins - yins + 1) |
-      (inSum << 1) |
-      ((inSum + yins) << 2) |
-      ((inSum + xins) << 4);
+                 (inSum << 1) |
+                 ((inSum + yins) << 2) |
+                 ((inSum + xins) << 4);
 
     let value = 0;
+    let c = kLookup[hash];
 
-    for (let c: Contribution2D | null = lookup[hash]; c; c = c.next) {
-      const dx = dx0 + c.dx;
-      const dy = dy0 + c.dy;
+    for (let i = 0; i < kContributionsPerHash; i++, c += kContributionsStride) {
+      const cdx = kContributions[c + 0];
+      const cdy = kContributions[c + 1];
+
+      const dx = dx0 + cdx;
+      const dy = dy0 + cdy;
 
       const attn = 2 - dx * dx - dy * dy;
       if (attn > 0) {
-        const px = xsb + c.xsb;
-        const py = ysb + c.ysb;
+        const cxsb = kContributions[c + 2];
+        const cysb = kContributions[c + 3];
 
-        const indexPartA = perm[px & 0xff];
-        const index = perm2D[(indexPartA + py) & 0xff];
+        const px = xsb + cxsb;
+        const py = ysb + cysb;
 
-        const valuePart = gradients2D[index] * dx + gradients2D[index + 1] * dy;
+        const indexPart = perm[px & 0xff];
+        const index = perm[(indexPart + py) & 0xff] & 0x0e;
 
+        const absGradientX = (index & 2) ? 2 : 5;
+        const absGradientY = 7 - absGradientX;
+        const gradientX = (index & 4) ? -absGradientX : absGradientX;
+        const gradientY = (index & 8) ? -absGradientY : absGradientY;
+
+        const valuePart = gradientX * dx + gradientY * dy;
         value += attn * attn * attn * attn * valuePart;
       }
     }
@@ -116,79 +85,64 @@ export function makeNoise2D(clientSeed: number): Noise2D {
 }
 
 const base2D = [
-  [1, 1, 0, 1, 0, 1, 0, 0, 0],
-  [1, 1, 0, 1, 0, 1, 2, 1, 1],
-];
-
-const gradients2D = [
-  5,
-  2,
-  2,
-  5,
-  -5,
-  2,
-  -2,
-  5,
-  5,
-  -2,
-  2,
-  -5,
-  -5,
-  -2,
-  -2,
-  -5,
+  [[1, 1, 0], [1, 0, 1], [0, 0, 0]],
+  [[1, 1, 0], [1, 0, 1], [2, 1, 1]],
 ];
 
 const lookupPairs2D = [
-  0,
-  1,
-  1,
-  0,
-  4,
-  1,
-  17,
-  0,
-  20,
-  2,
-  21,
-  2,
-  22,
-  5,
-  23,
-  5,
-  26,
-  4,
-  39,
-  3,
-  42,
-  4,
-  43,
-  3,
+  [0,  1],
+  [1,  0],
+  [4,  1],
+  [17, 0],
+  [20, 2],
+  [21, 2],
+  [22, 5],
+  [23, 5],
+  [26, 4],
+  [39, 3],
+  [42, 4],
+  [43, 3],
 ];
 
 const p2D = [
-  0,
-  0,
-  1,
-  -1,
-  0,
-  0,
-  -1,
-  1,
-  0,
-  2,
-  1,
-  1,
-  1,
-  2,
-  2,
-  0,
-  1,
-  2,
-  0,
-  2,
-  1,
-  0,
-  0,
-  0,
+  [0, 0, 1, -1],
+  [0, 0, -1, 1],
+  [0, 2, 1, 1],
+  [1, 2, 2, 0],
+  [1, 2, 0, 2],
+  [1, 0, 0, 0],
 ];
+
+const kContributionsStride = 4;
+const kContributionsPerHash = 4;
+
+// Stored packed: accessed at query time
+const [kContributions, kLookup] = (() => {
+  const n = p2D.length * kContributionsStride * kContributionsPerHash;
+  const contributions = new Float64Array(n);
+  let i = 0;
+
+  const append = (multiplier: number, xsb: number, ysb: number) => {
+    if (!(i + 4 <= contributions.length)) throw new Error();
+    contributions[i + 0] = -xsb - multiplier * SQUISH_2D;
+    contributions[i + 1] = -ysb - multiplier * SQUISH_2D;
+    contributions[i + 2] = xsb;
+    contributions[i + 3] = ysb;
+    i += 4;
+  };
+
+  for (const [baseIndex, multiplier, dx, dy] of p2D) {
+    for (const [bm, bx, by] of base2D[baseIndex]) {
+      append(bm, bx, by);
+    }
+    append(multiplier, dx, dy);
+  }
+  if (i !== contributions.length) throw new Error();
+
+  const lookup = new Int8Array(128);
+  lookup.fill(-1);
+  for (const [i, j] of lookupPairs2D) {
+    lookup[i] = kContributionsStride * kContributionsPerHash * j;
+  }
+  return [contributions, lookup];
+})();
